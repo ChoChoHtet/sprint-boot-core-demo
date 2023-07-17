1b3f23cc2c991e7b637def21932f911b81fcd4dd
Cho Cho Htet <michochohtet2017@gmail.com>
2023-07-17 23:07:58 +0700

    02-setter-injection

--- src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
+++ src/main/java/com/example/springcoredemo/SpringcoredemoApplication.java
@@ -3,6 +3,9 @@ package com.example.springcoredemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*@SpringBootApplication(
+		scanBasePackages = {"com.example.springcoredemo"}
+)*/
 @SpringBootApplication
 public class SpringcoredemoApplication {
 
--- src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -8,14 +8,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
     private Coach myCoach;
+
     //define a constructor for dependency injection
+   /*
     @Autowired
     public DemoController(Coach coach){
         myCoach = coach;
+    }*/
+
+    /**
+     * define setter injection
+     * can give any method name you want
+     */
+    @Autowired
+    public void setMyCoach(Coach myCoach) {
+        this.myCoach = myCoach;
     }
 
     @GetMapping("/dailyworkout")
-    public String getDailyWorkout(){
+    public String getDailyWorkout() {
         return myCoach.getDailyWorkout();
     }
 }
